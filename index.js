bad3df13fe3f0f737776fdbbd0c73439627432a9
Josh Gerontis <gerontisj@yahoo.com>
2021-06-24 10:31:09 -0600

    fixed port issue

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require("./server");
 const persist = require("./persist");
 
 // define a port
-const port = process.argv[2] || 8080;
+const port = process.argv[2] || process.env.PORT || 8080;
 
 // connect to the database
 persist.connect();
@@ -12,8 +12,10 @@ persist.connect();
 // once db is connected
 persist.onConnect(() => {
   // start the server
+  /*
   server.fileManager.openLogFile();
   server.syncLogAndDB();
+  */
 
   server.app.listen(port, () => {
     console.log(`Server Running on :${port}`);
